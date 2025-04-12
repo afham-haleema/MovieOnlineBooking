7a388c0115a09c7a918b8ca6009fb670cca117bd
afham-haleema <afhamhaleema@gmail.com>
2025-04-12 21:34:52 +0300

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";  
 $username = "root";       
-$password = "A39882987a@";    
+$password = "";    
 $dbname = "movies";         
 
 $conn = new mysqli($servername, $username, $password, $dbname);
